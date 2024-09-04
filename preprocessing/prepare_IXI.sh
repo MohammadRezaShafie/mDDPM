ff7b8e7591840897a06dfb386c7b81eb09057336
MohammadRezaShafie <mr.shafie7731@gmail.com>
2024-09-04 21:30:12 +0330

    change IXI to mri

--- preprocessing/prepare_IXI.sh
+++ preprocessing/prepare_IXI.sh
@@ -8,7 +8,7 @@ DATA_DIR=$2
 # make the arguments mandatory and that the data dir is not a relative path
 if [ -z "$INPUT_DIR" ] || [ -z "$DATA_DIR" ] 
 then
-  echo "Usage: ./prepare_IXI.sh <input_dir> <output_dir>"
+  echo "Usage: ./prepare_mri.sh <input_dir> <output_dir>"
   exit 1
 fi
 
@@ -19,30 +19,30 @@ then
 fi
 
 echo "Resample"
-mkdir -p $DATA_DIR/v1resampled/IXI/t2
-python resample.py -i $INPUT_DIR/t2 -o $DATA_DIR/v1resampled/IXI/t2 -r 1.0 1.0 1.0 
+mkdir -p $DATA_DIR/v1resampled/mri/t2
+python resample.py -i $INPUT_DIR/t2 -o $DATA_DIR/v1resampled/mri/t2 -r 1.0 1.0 1.0 
 # rename files for standard naming
-for file in $DATA_DIR/v1resampled/IXI/t2/*
+for file in $DATA_DIR/v1resampled/mri/t2/*
 do
   mv "$file" "${file%-T2.nii.gz}_t2.nii.gz"
 done
 
 echo "Generate masks"
-CUDA_VISIBLE_DEVICES=0 hd-bet -i $DATA_DIR/v1resampled/IXI/t2 -o $DATA_DIR/v2skullstripped/IXI/t2 
-python extract_masks.py -i $DATA_DIR/v2skullstripped/IXI/t2 -o $DATA_DIR/v2skullstripped/IXI/mask
-python replace.py -i $DATA_DIR/v2skullstripped/IXI/mask -s " _t2" ""
+CUDA_VISIBLE_DEVICES=0 hd-bet -i $DATA_DIR/v1resampled/mri/t2 -o $DATA_DIR/v2skullstripped/mri/t2 
+python extract_masks.py -i $DATA_DIR/v2skullstripped/mri/t2 -o $DATA_DIR/v2skullstripped/mri/mask
+python replace.py -i $DATA_DIR/v2skullstripped/mri/mask -s " _t2" ""
 
 echo "Register t2"
-python registration.py -i $DATA_DIR/v2skullstripped/IXI/t2 -o $DATA_DIR/v3registered_non_iso/IXI/t2 --modality=_t2 -trans Affine -templ sri_atlas/templates/T1_brain.nii
+python registration.py -i $DATA_DIR/v2skullstripped/mri/t2 -o $DATA_DIR/v3registered_non_iso/mri/t2 --modality=_t2 -trans Affine -templ sri_atlas/templates/T1_brain.nii
 
 echo "Cut to brain"
-python cut.py -i $DATA_DIR/v3registered_non_iso/IXI/t2 -m $DATA_DIR/v3registered_non_iso/IXI/mask/ -o $DATA_DIR/v3registered_non_iso_cut/IXI/ -mode t2
+python cut.py -i $DATA_DIR/v3registered_non_iso/mri/t2 -m $DATA_DIR/v3registered_non_iso/mri/mask/ -o $DATA_DIR/v3registered_non_iso_cut/mri/ -mode t2
 
 echo "Bias Field Correction"
-python n4filter.py -i $DATA_DIR/v3registered_non_iso_cut/IXI/t2 -o $DATA_DIR/v4correctedN4_non_iso_cut/IXI/t2 -m $DATA_DIR/v3registered_non_iso_cut/IXI/mask
+python n4filter.py -i $DATA_DIR/v3registered_non_iso_cut/mri/t2 -o $DATA_DIR/v4correctedN4_non_iso_cut/mri/t2 -m $DATA_DIR/v3registered_non_iso_cut/mri/mask
 
-mkdir $DATA_DIR/v4correctedN4_non_iso_cut/IXI/mask
-cp $DATA_DIR/v3registered_non_iso_cut/IXI/mask/* $DATA_DIR/v4correctedN4_non_iso_cut/IXI/mask
+mkdir $DATA_DIR/v4correctedN4_non_iso_cut/mri/mask
+cp $DATA_DIR/v3registered_non_iso_cut/mri/mask/* $DATA_DIR/v4correctedN4_non_iso_cut/mri/mask
 echo "Done"
 
 # now, you should copy the files in the output directory to the data directory of the project
